dc6514581bae4cf685cd2cb8e13b0c48b1a4ace0
Nicholas Fielder <46355174+fieldter@users.noreply.github.com>
2022-09-28 12:07:38 -0400

    Fake rest API for testing 2

--- src/components/test/FriendsList.tsx
+++ src/components/test/FriendsList.tsx
@@ -5,9 +5,9 @@ import { AxiosResponse } from 'axios'
 type Props = {}
 
 const FriendsList = (props: Props) => {
-    async function getFriends(){
-        await FAKEWORDS_API.get('getFriends')
-            .then(async (response: AxiosResponse) => {
+    function getFriends(){
+        FAKEWORDS_API.get('getFriends')
+            .then((response: AxiosResponse) => {
                 console.log(response.data)
             })
     }
