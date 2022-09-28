0ad99690b0b2e722911854f199c90e0c21e368a5
Nicholas Fielder <46355174+fieldter@users.noreply.github.com>
2022-09-28 15:24:04 -0400

    Friends List

--- src/components/test/FriendsList.tsx
+++ src/components/test/FriendsList.tsx
@@ -1,20 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FAKEWORDS_API from '../../utils/FakeApiConfig'
 import { AxiosResponse } from 'axios'
+import { get } from 'https'
+
+import '../../css/friendlist.css'
 
 type Props = {}
 
 const FriendsList = (props: Props) => {
+    const [isShown, setIsShown] = useState(false);
+    getFriends()
     function getFriends(){
         FAKEWORDS_API.get('getFriends')
             .then((response: AxiosResponse) => {
-                console.log(response.data)
+                sessionStorage.setItem('friends',JSON.stringify(response.data))
             })
     }
+
+    const friends = JSON.parse(sessionStorage.friends)
+
+    const friendslist = []
+    const pendinglist = []
+    for (let i = 0; i < friends.length; i++) {
+        if (!friends[i].pending){
+            friendslist.push(
+                <div id='flrow'>
+                    <div className='friend'>{friends[i].name}</div><button>Accept</button><button>Dismiss</button>
+                </div>
+            ) 
+        }else{
+            pendinglist.push(
+                <div id='flrow'>
+                    <div className='friend'>{friends[i].name}</div><button>Chat</button><button>Unfriend</button>
+                </div>
+            )
+        }
+    }
+
+
   return (
-    <>
-          <div>FriendsList</div>
-          <button onClick={()=>(getFriends())}>getFriends</button>
+    <><div id='flall'>
+          <div onClick={() => (setIsShown(!isShown))} id='fldiv' className='simple'>
+              <div style={{display: isShown?'flex':'none'}} id='floverlay'>
+                  <div className='frtitle'>Friend Requests</div>
+                
+                  <div id='flcontainer'>
+                      <div id='fl'></div>
+                      {friendslist}
+                  </div>
+                  <div className='frtitle'>Friends</div>
+                  <div id='flpcontainer'>
+                      <div id='fl'></div>
+                      {pendinglist}
+                  </div>
+              </div>            
+          </div>
+      </div>
+            
+          
     </>
     
   )
