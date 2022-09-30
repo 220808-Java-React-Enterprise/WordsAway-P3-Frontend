02490d4b2a6356c178a8d3baa75ad789f2f9cc78
Nathan <tahgtahv@gmail.com>
2022-09-30 10:40:42 -0400

    maybe fix regex

--- jest.config.ts
+++ jest.config.ts
@@ -27,7 +27,7 @@ export default {
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
-    "\\node_modules\\"
+    "\\\\node_modules\\\\"
   ],
 
   // Indicates which provider should be used to instrument code for coverage
@@ -169,7 +169,7 @@ export default {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
-    "\\node_modules\\"
+    "\\\\node_modules\\\\"
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
@@ -186,7 +186,7 @@ export default {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-    "\\node_modules\\",
+    "\\\\node_modules\\\\",
     "\\.pnp\\.[^\\\\]+$"
   ],
 
