acca7d1b68e2a16a813f872912c0440478b72719
Leighton Albrecht <leightonalbrecht@gmail.com>
2022-10-03 15:31:14 -0400

    fixed unmarked merge conflict

--- src/types/User.type.ts
+++ src/types/User.type.ts
@@ -1,12 +1,9 @@
 export type User = {
   username: string
-<<<<<<< HEAD
   board_id: string
   is_cpu: boolean
-=======
   elo: number
   gamesPlayed: number
   gamesWon: number
   avatar: number
->>>>>>> 33049bc7704bda1858f42a5962c003951db90457
 }
