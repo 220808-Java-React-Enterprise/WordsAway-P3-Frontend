62799cbdc245f9202b1b91a0c09b5623630e1906
Koukaakiva <koukaakiva@gmail.com>
2022-10-06 12:28:57 -0400

    Fixed multiple socket bug.

---
+++ src/types/Message.type.ts
@@ -0,0 +1,9 @@
+export default class MessageType {
+  static LOGIN = 'LOGIN'
+  static START_CHAT = 'START_CHAT'
+  static START_CHAT_ACK = 'START_CHAT_ACK'
+  static ADD_USER = 'ADD_USER'
+  static MESSAGE = 'MESSAGE'
+  static LEAVE_CHAT = 'LEAVE_CHAT'
+  static LEAVE_CHAT_ACK = 'LEAVE_CHAT_ACK'
+}
