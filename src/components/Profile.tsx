151f465d3d9c0284bac942ba1b904fae739f2dcc
mlopez44 <60417513+mlopez44@users.noreply.github.com>
2022-10-06 12:14:08 -0400

    friend on profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -12,19 +12,73 @@ interface UserProp{
 
 
 export default function Profile({profileUser}: UserProp){
- console.log(profileUser?.username);
+ const [users, setUsers] = useState<User[]>([]);
+ const [isShown2, setIsShown2] = useState(false);
+ const [unfriendName, setUnfriendName] = useState("")
+ var friends: { outgoingRequests: any[], incomingRequests: any[], friends: any[] } = { outgoingRequests: [], incomingRequests: [], friends: []}
+
+ useEffect(() => {
+  getFriends()
+}, [])
   
+async function getFriends() {
+  await WORDS_API.get('/getFriendsList')
+  .then((response: AxiosResponse<User[]>) => {
+    console.log(response.data)
+    setUsers(response.data)
+    sessionStorage.setItem('friends', JSON.stringify(response.data))
+    
+    console.log(friends)
+    
+  })
+  .catch(() => (window.location.href = '/login'))
+}
+
   async function addFriend(){
     console.log(profileUser?.username);
     await WORDS_API.post('addFriend',{}, {
       params: { username: profileUser?.username } 
     })
-    .then()
+    .then((response) => {
+      window.location.reload();
+    })
     .catch((response) => alert(response))
 
-    console.log("Friend added!");
   }
 
+  async function removeFriend(){
+    console.log(profileUser?.username);
+    await WORDS_API.post('removeFriend',{}, {
+      params: { username: profileUser?.username } 
+    })
+    .then((response) => {
+      window.location.reload();
+    })
+    .catch((response) => alert(response))
+
+  }
+
+
+  function removeFriendPrompt() {
+    setIsShown2(true)
+    console.log("UNFRIEND Pending");
+}
+
+  async function cancelRequest(){
+    console.log(profileUser?.username);
+    await WORDS_API.post('cancelFriend',{}, {
+      params: { username: profileUser?.username } 
+    })
+    .then((response) => {
+      window.location.reload();
+    })
+    .catch((response) => alert(response))
+
+  }
+  friends = JSON.parse(sessionStorage.friends)
+  console.log("prof: " + JSON.stringify(profileUser))
+  console.log("requests: " + friends?.outgoingRequests)
+  console.log("status: " + friends.outgoingRequests.includes(profileUser))
   return (
     <div className ="profile">
 
@@ -42,7 +96,7 @@ export default function Profile({profileUser}: UserProp){
 </div>
 
 <div className = "addfriend">
-{profileUser?.username == sessionStorage.getItem("username") ? <></> : <button onClick={addFriend}>Add Friend</button>}
+{profileUser?.username == sessionStorage.getItem("username") ? <></> : JSON.stringify(friends?.friends).includes(JSON.stringify(profileUser)) ? <button onClick={removeFriendPrompt}>Remove Friend</button> : JSON.stringify(friends?.outgoingRequests).includes(JSON.stringify(profileUser)) ? <button onClick={cancelRequest}>Cancel Friend Request</button> : <button onClick={addFriend}>Add Friend</button> }
 
 </div>
 </div>
@@ -50,6 +104,8 @@ export default function Profile({profileUser}: UserProp){
 
 
 <div className="rest">
+
+
 
 <div className="history">
 <div className = "winrate">
@@ -70,7 +126,11 @@ export default function Profile({profileUser}: UserProp){
 </div>
 </div>
 
-
+<div style={{ display: (isShown2) ? 'flex' : 'none' }} id='removeConfirm'>
+                <div>
+                    <div>Are You sure You want to delete {profileUser?.username} from your friends List? <div><button id='confirm-uf-yes' onClick={() => { removeFriend(); setIsShown2(false) }}>Yes</button><button id='confirm-uf-no' onClick={() => (setIsShown2(false))}>No</button></div></div>
+                </div>
+            </div>  
 
 
 
